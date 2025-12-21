112dacce27e6b107edebf3c65bfd238bd7d52f62
ludekcz <dvorakludekcz@seznam.cz>
2025-12-21 10:12:11 +0100

    feat: backend&frontend modules

---
+++ backend/lol-stats-backend/src/main/java/cz/dvorak/lolstatsbackend/LolStatsBackendApplication.java
@@ -0,0 +1,13 @@
+package cz.dvorak.lolstatsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolStatsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LolStatsBackendApplication.class, args);
+    }
+
+}
---
+++ backend/lol-stats-backend/src/test/java/cz/dvorak/lolstatsbackend/LolStatsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.dvorak.lolstatsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LolStatsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
