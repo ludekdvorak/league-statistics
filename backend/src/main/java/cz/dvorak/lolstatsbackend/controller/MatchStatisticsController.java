f20288cad37893343b175963a10e7f4c32a89f22
ludekcz <dvorakludekcz@seznam.cz>
2025-12-29 19:04:10 +0100

    fix: newStructureTestedBothServersCompiling

---
+++ backend/src/main/java/cz/dvorak/lolstatsbackend/controller/MatchStatisticsController.java
@@ -0,0 +1,15 @@
+package cz.dvorak.lolstatsbackend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class MatchStatisticsController {
+
+    @GetMapping
+    public String test() {
+      return "OK";
+    }
+}
