d0bbb5e3d3db43f5c10aae253cd5bf812f2bab26
ludekcz <dvorakludekcz@seznam.cz>
2025-12-31 17:35:29 +0100

    fix: README Api

--- backend/src/main/java/cz/dvorak/lolstatsbackend/service/MatchStatisticsService.java
+++ backend/src/main/java/cz/dvorak/lolstatsbackend/service/MatchStatisticsService.java
@@ -6,10 +6,18 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class MatchStatisticsService {
+
+    // -- README --
+    // 1st https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/GLAVIAS/EUNE?api_key=xx
+    // use puuid
+    // 2nd https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/zrlja7hX5SKhn_83gSJSLjpV4iSQRwkfCLp_AAs_izsNgi4x6eYEe5_juETx2FhSzoCJCud9iAPS0Q/ids?api_key=xx
+    // use 20 match ids to find details
+    // 3rd https://europe.api.riotgames.com/lol/match/v5/matches/EUN1_3882220677?api_key=xx
+    // find detail information about matches
+
     public MatchStatisticsResponseDto getMatchStatistics(MatchStatisticsRequestDto request)
     {
 
-        // https://eun1.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=
 
         return null;
     }
